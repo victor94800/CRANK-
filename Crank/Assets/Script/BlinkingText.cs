a76683434459552e1c68de86431369381c613e85
THEM31 <mathis.vatble@gmail.com>
2017-03-28 01:47:39 +0200

    insertion de l'intro finale ( je pense)

---
+++ Crank/Assets/Script/BlinkingText.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BlinkingText : MonoBehaviour
+{
+    //public GameObject toactivate;
+    //public GameObject todisable;
+    Text flashingText;
+
+    void Start()
+    {
+        
+        flashingText = GetComponent<Text>();
+        
+        StartCoroutine(BlinkText());
+    }
+
+
+    public IEnumerator BlinkText()
+    {
+
+        while (!(Input.GetKeyDown(KeyCode.Space) || Input.GetKey("joystick button 7")))
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKey("joystick button 7"))
+            {
+
+                flashingText.enabled = false;
+                //toactivate.SetActive(true);
+            }
+            flashingText.text = "";
+
+            yield return new WaitForSeconds(.7f);
+
+            flashingText.text = "Press start/enter!";
+            yield return new WaitForSeconds(.5f);
+            
+        }
+    
+        
+    }
+
+}
