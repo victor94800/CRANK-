5651ddc289e76a3d12b8e5e3bb56089eb53de34f
THEM31 <mathis.vatble@gmail.com>
2017-04-20 01:46:50 +0200

    SEND NUDES

---
+++ Crank/Assets/Script/GlobShop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobShop : MonoBehaviour {
+    public int money;
+    public bool ISfireswordallowed;
+    public bool isthunderswordallowed;
+    public bool isdoublejumpallowed;
+    // Use this for initialization
+    void Start()
+    {
+        money = 100;
+        ISfireswordallowed = false;
+        isthunderswordallowed = false;
+        isdoublejumpallowed = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
