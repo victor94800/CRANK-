9e0887c32095994da7eb03eb0590a3421f6c9a23
THEM31 <mathis.vatble@gmail.com>
2017-04-24 21:50:09 +0200

    suprise:D

---
+++ Crank/Assets/Script/OpenClosePhone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenClosePhone : MonoBehaviour
+{
+
+    public GameObject Phone;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown("joystick button 6"))
+        {
+            phone();
+        }
+    }
+    public void phone()
+    {
+        if (Phone.activeInHierarchy == false)
+        {
+            Phone.SetActive(true);
+            
+        }
+        else
+        {
+            Phone.SetActive(false);
+            
+        }
+    }
+
+}
+
