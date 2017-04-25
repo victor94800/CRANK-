8556bedc98881bbf1aa59553c8dfe8e114699f7a
victor94800 <victor.tregouet33@gmail.com>
2017-04-25 03:57:48 +0200

    azeifjoigjoazqijhg^pzojig

--- Crank/Assets/Script/Mission_1.cs
+++ Crank/Assets/Script/Mission_1.cs
@@ -6,11 +6,16 @@ public class Mission_1 : MonoBehaviour {
 	bool is_jump = false;
 	bool is_Etuto_Deafed = false;
 	GameObject Etuto;
+	public GameObject[] checkpoints;
 	// Use this for initialization
 	void Start ()
 	{
 		Etuto = GameObject.Find("Etuto");
-		Etuto.SetActive(false);
+		//Etuto.SetActive(false);
+		foreach(GameObject i in checkpoints)
+		{
+			i.SetActive(false);
+		}
 	}
 	
 	// Update is called once per frame
@@ -21,7 +26,7 @@ public class Mission_1 : MonoBehaviour {
 		{
 			Etuto.SetActive(true);
 		}
-		is_Etuto_Deafed = Etuto.active;
+		//is_Etuto_Deafed = Etuto.active;
 		if (is_Etuto_Deafed )
 		{
 			print("won");
