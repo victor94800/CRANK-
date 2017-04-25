1e0def50d7a5a84d003a1bf4faafdaf85d355ed7
victor94800 <victor.tregouet33@gmail.com>
2017-04-25 23:51:17 +0200

    mission

---
+++ Crank/Assets/Script/mission1_1_2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mission1_1_2 : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject player;
+	public GameObject global;
+	public int heigh;
+
+	void Start ()
+	{
+		global = GameObject.Find("Global");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (player.transform.position.y >= heigh)
+		{
+			GetComponent<mission_1_2>().enabled = true;
+			GameObject.Find("GameController").GetComponent<GameController>().mission = 2;
+			GetComponent<mission_1_2>().enabled = true;
+			Destroy(this);
+		}
+		
+	}
+}
--- Crank/Assets/Script/mission_1_1.cs
+++ Crank/Assets/Script/mission_1_1.cs
@@ -6,21 +6,40 @@ public class mission_1_1 : MonoBehaviour {
 	public GameObject player;
 	public AudioClip m_1_1;
 	public float height;
+	public GameObject phone;
+	public GameObject phonecamera;
+	public GameObject camera_target;
+	private AudioSource audi;
 
 	// Use this for initialization
 	void Start ()
 	{
+
 		GetComponent<AudioSource>().PlayOneShot(m_1_1);
+		
+		phone.SetActive(true);
+		GameObject.Find("Camera2").SetActive(true);
+		GameObject.Find("Camera").SetActive(false);
+		GameObject.Find("Camera2").GetComponent<phonecamera>().target_p = camera_target;
+		GameObject.Find("apps").SetActive(false);
+		GameObject.Find("PhoneBackground").SetActive(false);
+		phonecamera.GetComponent<Camera>().fieldOfView = 10;
+
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
 	{
-		if (player.transform.position.y >= height)
+		if (!audi.isPlaying)
 		{
-			// GetComponent<mission_1_2>().enabled = true;
+			phonecamera.GetComponent<Camera>().fieldOfView = 60;
+			GameObject.Find("Intello").SetActive(false);
+			phone.SetActive(false);
+			GameObject.Find("Camera1").SetActive(true);
+			GameObject.Find("Camera2").GetComponent<phonecamera>().target_p = null;
+			GameObject.Find("Camera2").SetActive(false);
+			
 			Destroy(this);
-
 		}
 	}
 }
